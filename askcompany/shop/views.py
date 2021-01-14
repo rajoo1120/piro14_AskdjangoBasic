21e4bb0916b6636b1ec86ae283b40e8abd5c675c
heejin <dongdong7251@naver.com>
2021-01-14 21:28:25 +0900

    5강

---
+++ askcompany/shop/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def archives_year(request,year):
+    return HttpResponse('{}년도에 대한 내용'.format(year)) 
