8c106435d07a3b98912c8047b9dce9ba3aa54634
heejin <dongdong7251@naver.com>
2021-01-15 12:46:27 +0900

    14-15강

--- askcompany/blog/models.py
+++ askcompany/blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model) :
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     message = models.TextField()
+    
--- askcompany/shop/models.py
+++ askcompany/shop/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.conf import settings
+
+class Post(models.Model) :
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
 class Item(models.Model):
     name=models.CharField(max_length=100)
