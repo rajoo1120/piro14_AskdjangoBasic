f62c0c6047bd93a63886b6522f4823f24dc6fffe
heejin <dongdong7251@naver.com>
2021-01-17 17:32:01 +0900

    26강

--- askcompany/askcompany/urls.py
+++ askcompany/askcompany/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -9,6 +10,8 @@ urlpatterns = [
     path('shop/',include('shop.urls')),
     path('blog/', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 # if settings.DEBUG:
 #     import debug_toolbar
