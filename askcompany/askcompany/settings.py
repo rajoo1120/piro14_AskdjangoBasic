45a5c5f6defb44bf4b13bdbefb68c12002e47b8e
heejin <dongdong7251@naver.com>
2021-01-17 15:06:22 +0900

    22강

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'shop',
-    'django_extensions',
     'blog',
     # 'debug_toolbar',
 ]
