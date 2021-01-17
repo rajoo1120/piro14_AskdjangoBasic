c3dcf08cc7eccaf4954b55844e486b1d940b3109
heejin <dongdong7251@naver.com>
2021-01-17 16:20:28 +0900

    23-25강. 커밋이 꼬여서 밑 커밋들 중 23강 커밋만 22강 커밋으로 보시면 됩니다

--- askcompany/askcompany/settings.py
+++ askcompany/askcompany/settings.py
@@ -27,7 +27,6 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -125,4 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'askcompany','static'),
+]
+
 # INTERNAL_IPS=['127.0.0.1']
