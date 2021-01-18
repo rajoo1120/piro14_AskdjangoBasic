37992333f4894a3c3b1867cdd9c4e39c435c2ecc
heejin <dongdong7251@naver.com>
2021-01-18 15:25:44 +0900

    (중급편)1강

--- askcompany/askcompany/urls.py
+++ askcompany/askcompany/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/',include('shop.urls')),
     path('blog/', include('blog.urls')),
+     path('', lambda req: redirect('/blog/')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- askcompany/blog/models.py
+++ askcompany/blog/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-from askcompany.utils import uuid_upload_to
 
-class Post(models.Model) :
-    author_name = models.CharField(max_length=20)
+
+class Post(models.Model):
     title = models.CharField(max_length=100)
-    content= models.TextField()
-    photo= models.ImageField(upload_to=uuid_upload_to)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    message = models.TextField()
-    
+    def __str__(self):
+        return self.title
